5c7c9a7cf5ed6788bb105cc065514dfae3dcaf8a
Michal <michalgrzegorczyk04@gmail.com>
2022-01-26 21:00:27 +0100

    Add Sidebar and NavBar events

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,8 @@ import { Link } from 'gatsby'
 import NavLink from './NavLink'
 import { GatsbyContext } from '../context/context'
 const Navbar = () => {
+  const { isSidebarOpen, showSidebar } = useContext(GatsbyContext)
+
   return (
     <Wrapper>
       <div className="nav-center">
@@ -13,9 +15,11 @@ const Navbar = () => {
           <Link to="/">
             <img src={logo} alt="design" />
           </Link>
-          <button className="toggle-btn">
-            <GoThreeBars />
-          </button>
+          {!isSidebarOpen &&
+            <button className="toggle-btn" onClick={showSidebar}>
+              <GoThreeBars />
+            </button>
+          }
         </div>
         <ul className="nav-links">
           <li>
@@ -25,7 +29,7 @@ const Navbar = () => {
             <button>developers</button>
           </li>
           <li>
-            <button>company</button>
+            <Link to="products/payments">company</Link>
           </li>
         </ul>
       </div>
